d4f1d09904accea453a5bc6bd62cce81fda7d123
Gustavo Luna <gustavolunalimon@gmail.com>
2025-01-14 16:54:23 -0600

    [add] header route link

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react'
+import { useRouter } from "next/router";
 
 export default function Header() {
+	const route = useRouter()
 	const [menuStatus, setMenuStatus] = useState(false);
 	useEffect(() => {
 		window.addEventListener('scroll', function() {
@@ -25,10 +27,10 @@ export default function Header() {
 				</div>
 				<div id="navbarBasicExample" className={`navbar-menu ${menuStatus ? 'is-active' : ''}`}>
 					<div className="navbar-end">
-						<a className="navbar-item"><b>00</b>Home</a>
-						<a className="navbar-item"><b>01</b>destination</a>
-						<a className="navbar-item"><b>02</b>crew</a>
-						<a className="navbar-item"><b>03</b>technology</a>
+						<a className={`navbar-item ${ route.asPath === '/' ? 'is-active' : '' }`} href="/"><b>00</b>Home</a>
+						<a className={`navbar-item ${ route.asPath === '/destination' ? 'is-active' : '' }`} href="/destination"><b>01</b>destination</a>
+						<a className={`navbar-item ${ route.asPath === '/crew' ? 'is-active' : '' }`} href="/crew"><b>02</b>crew</a>
+						<a className={`navbar-item ${ route.asPath === '/technology' ? 'is-active' : '' }`} href="/technology"><b>03</b>technology</a>
 					</div>
 				</div>
 			</nav>
