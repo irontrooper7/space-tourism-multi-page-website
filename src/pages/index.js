7db7e79f5933022337897f9b0438851702fc7b91
Gustavo Luna <gustavolunalimon@gmail.com>
2025-01-14 16:01:27 -0600

    [add] home styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,19 @@ import Layout from "../../components/Layout";
 export default function Home() {
 	return (
 		<Layout>
-			<section className="hero is-fullheight">
+			<section className="home hero is-fullheight">
 				<div className="hero-body">
 					<div className="container">
-						<div className="columns">
+						<div className="columns is-gapless">
 							<div className="column">
-								<h1>Space</h1>
-								<p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+								<div className="cont-info">
+									<h5>SO, YOU WANT TO TRAVEL TO</h5>
+									<h1>Space</h1>
+									<p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+								</div>
 							</div>
 							<div className="column">
-								<button>Explore</button>
+								<button className="button">Explore</button>
 							</div>
 						</div>
 					</div>
